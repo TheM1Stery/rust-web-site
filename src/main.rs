7ffdb781fee61499b7e13a857e9369b9b7f53e2f
Seymur Bagirov <megadestroyer230@gmail.com>
2024-04-10 22:51:54 +0400

    add tracing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use axum::{response::Html, routing::get, Json, Router};
 use serde::Serialize;
 use tokio::signal;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
 
 
 async fn index() -> Html<&'static str> {
@@ -24,10 +27,23 @@ async fn return_json() -> Json<Test> {
 
 #[tokio::main]
 async fn main() {
-    let port = 1337;
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                // axum logs rejections from built-in extractors with the `axum::rejection`
+                // target, at `TRACE` level. `axum::rejection=trace` enables showing those events
+                "example_tracing_aka_logging=debug,tower_http=debug,axum::rejection=trace".into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+
+    let port = 5000;
     let router = Router::new()
                     .route("/", get(index))
-                    .route("/json", get(return_json));
+                    .route("/json", get(return_json))
+                    .layer(TraceLayer::new_for_http());
 
 
 
